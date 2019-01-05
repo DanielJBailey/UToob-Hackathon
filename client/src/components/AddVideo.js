3fb8fddf8480096570ec2dfbb8278a89f84fdf86
Crys <sovereigncrys88@gmail.com>
2019-01-05 11:10:58 -0700

    Add Video Component

--- client/src/components/AddVideo.js
+++ client/src/components/AddVideo.js
@@ -65,5 +65,31 @@ class AddVideo extends React.Component {
     }
 
 }
+<div class="custom-select" style="width:200px;">
+  <select>
+    <option value="0">Action</option>
+    <option value="1">Comedy</option>
+    <option value="2">BMW</option>
+    <option value="3">Citroen</option>
+    <option value="4">Ford</option>
+    <option value="5">Honda</option>
+    <option value="6">Jaguar</option>
+    <option value="7">Land Rover</option>
+    <option value="8">Mercedes</option>
+    <option value="9">Mini</option>
+    <option value="10">Nissan</option>
+    <option value="11">Toyota</option>
+    <option value="12">Volvo</option>
+  </select>
+</div>
+
+{/* <Form.Input 
+    name="genre"
+    label="Genre"
+    placeholder="Genre"
+    required
+    value={genre}
+    onChange={this.handleChange}
+/> */}
 
 export default AddVideo;
